eb1d4a23320fb4fc585180df8b5dbaeb7b524b4c
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-22 15:42:01 +0200

    some chenges

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,15 @@
 // export const Person = ({ person }) => ();
 
 export const Person = ({ person }) => {
+  const {name, age, sex, isMarried, partnerName} = person;
+  const partnerLabel = sex === 'm' ? 'wife' : 'husband';
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
-      {person.isMarried === true ? (
+      <h2 className="Person__name">My name is {name}</h2>
+      {age ? <p className="Person__age">I am {age}</p> : null}
+      {isMarried === true ? (
         <p className="Person__partner">
-          {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+          {partnerName} is my {partnerLabel}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
